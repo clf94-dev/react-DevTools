20e15c7ce8a4afb270b7f43e51cf540cb45461d0
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-03 21:52:27 +0200

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Info from "./Info";
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Info value={props.tel} />
+        <Info value={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Info.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Info(props) {
+  return <p className="info">{props.value}</p>;
+}
+export default Info;
